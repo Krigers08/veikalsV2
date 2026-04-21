4d790b1ccb93380e7d0dc5a8d20d243e1d760741
me <me@me.me>
2026-04-21 13:42:25 +0300

    moved client display function to the coresponding class

--- src/controllers/CustomerController.php
+++ src/controllers/CustomerController.php
@@ -1,5 +1,16 @@
 <?php
 class CustomerController
 {
+    public static function index($conn)
+    {
+        $stmt = $conn->query("SELECT * FROM customers");
+        $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+        echo "<h1>Klientu saraksts</h1>";
+        echo "<ul>";
+        foreach ($customers as $customer) {
+            echo "<li>" . htmlspecialchars($customer['name']) . " - " . htmlspecialchars($customer['email']) . "</li>";
+        }
+        echo "</ul>";
+    }
 }
